c05f033c095d4d3587e01a7250338e3f7a648de5
Peggy Sturman <glamazon5@gmail.com>
2019-12-21 11:47:13 -0800

    pushing code before deployment to Heroku

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 const express = require('express')
-
 const morgan = require('morgan')
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 const helmet = require('helmet')
 const cors = require('cors')
 const MOVIES = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -46,8 +47,22 @@ app.get('/movie', function handleGetMovie(req, res) {
   res.json(response)
 })
 
-const PORT = 9000
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+
+
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  
 })
